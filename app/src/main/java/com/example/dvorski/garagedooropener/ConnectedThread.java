f00c50ae3120db0088607a2c06e202267fd825ae
dvorski <dadeky@gmail.com>
2015-07-31 14:01:52 +0200

    Removed unused imports

--- app/src/main/java/com/example/dvorski/garagedooropener/ConnectedThread.java
+++ app/src/main/java/com/example/dvorski/garagedooropener/ConnectedThread.java
@@ -2,16 +2,13 @@ package com.example.dvorski.garagedooropener;
 
 
 import android.bluetooth.BluetoothSocket;
-import android.os.Handler;
 import android.os.Bundle;
+import android.os.Handler;
 import android.os.Message;
 
-import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.io.OutputStream;
-import java.lang.String;
 
 /**
  * Created by dvorski on 23.2.2015..
